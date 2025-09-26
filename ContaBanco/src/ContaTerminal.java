adae39fa5b1ffe32449669452da0d596edfbb732
João <joaomalaquias2006@gmial.com>
2025-09-26 17:18:51 -0400

    Simulação de uma pseudo conta bancaria pedido ao usuario dados e mostrando os dados que o usuario digitou

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //ToDO: Conhecer e importar a classe Scanner
+        // Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        // Exibir a mensagem conta criada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Por favor digite o número da agência");
+        String numeroAgencia = teclado.nextLine();
+        // ler o numero da agencia digitado pelo usuario
+        System.out.println("Digite o número de sua conta");
+        int numeroConta = teclado.nextInt();
+        // ler o numero da conta digitado pelo usuario
+        teclado.nextLine();
+        //para não bugar o Scanner
+        System.out.println("Digite seu nome completo");
+        String nome = teclado.nextLine();
+        //ler o nome digitado pelo usuario
+        System.out.println("Digite seu saldo");
+        double saldo = teclado.nextDouble();
+        //ler o saldo digitado pelo usuario
+        System.out.println("Olá " + nome + " , obrigado por criar uma conta em nosso banco sua  "+ numeroAgencia + " , conta "+ numeroConta + " e seu saldo "+ saldo + " já está disponível para saque");
+        // resultado final    
+        teclado.close();
+
     }
 }
